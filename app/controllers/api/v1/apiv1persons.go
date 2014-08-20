f273c03af6ecf34df64faa9840a4e305db80e897
Oscar Swanros <swanros@gmail.com>
2014-08-19 19:10:58 -0500

    Refactored code to make routing easier for controllers that may be added later

--- app/controllers/api/v1/apiv1persons.go
+++ app/controllers/api/v1/apiv1persons.go
@@ -6,27 +6,27 @@ import (
 	"github.com/revel/revel"
 )
 
-type API_V1 struct {
+type APIV1Persons struct {
 	*revel.Controller
 }
 
-func (c API_V1) Index() revel.Result {
+func (c APIV1Persons) Index() revel.Result {
 	persons := models.AllPersons()
 
 	return c.RenderJson(persons)
 }
 
-func (c API_V1) Show(id int64) revel.Result {
+func (c APIV1Persons) Show(id int64) revel.Result {
 	person := models.FindPerson(id)
 
 	return c.RenderJson(person)
 }
 
-func (c API_V1) Create(person models.Person) revel.Result {
+func (c APIV1Persons) Create(person models.Person) revel.Result {
 	models.CreatePerson(person)
 
 	response := make(map[string]string)
-	response["message"] = "Record created successfully."
+	response["message"] = "Record created successfuly."
 
 	return c.RenderJson(response)
 }
